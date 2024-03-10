1b592058d4c6fa8bbc45db98b48105b4c7230279
kibongos40 <kibongos40@gmail.com>
2024-03-10 14:22:53 +0200

    Merge branch 'develop' into ft-dashboard

--- dashboard/assets/js/script.js
+++ dashboard/assets/js/script.js
@@ -8,6 +8,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 let loading = `
 	<div class="loader">
 		<div class="load"></div>
